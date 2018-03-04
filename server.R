310371985eb80455343e5211bb02d57d17c8b56e
Kangwoo Choi <kangwooc@gmail.com>
2018-03-04 14:58:02 -0800

    fix the image

--- server.R
+++ server.R
@@ -2,15 +2,8 @@
 library(shiny)
 
 server <- function(input, output, session) {
-  output$logoImage <- renderImage({
-    list(src = "data/ad62.png",
-         contentType = "image/png",
-         width = 200,
-         height = 200,
-         alt = "logo",
-         align = "center"
-         )
-  })  
+  
+   
 }
 
 shinyServer(server)
--- ui.R
+++ ui.R
@@ -3,37 +3,43 @@ library(maptools)
 library(dplyr)
 library(shiny)
 
-ui <- navbarPage("Oil Spill Accident",
-        tabPanel("OverView",
-                 imageOutput("logoImage"),
-                 mainPanel(
-                   h3("Overview of our report"),
-                   p("For the final project, we are working with data of Oil Pipeline Accidents and/or spills in the United
-                      States, from 2010 to present day. The data references the locations of the spills, the corporations
-                     managing the pipelines, causes for the spills, the total costs resulting in these spills, among other
-                     factors. The data used was collected and published by the", a(href = 'https://www.kaggle.com/usdot/pipeline-accidents', "US Department of Transportation", ".")),
-                   p("The data is focused on past spills, the basic details about the spills, including location, date, time,
-                     loss of oil and the impact (in terms of USD) of the oil spills. Looking through the data allows us
-                     insights into patterns and trends of the oil spills and provide a monetary glimpse of their impact. In
-                     learning these things, we will be able to provide to our clients, specific data about oil spills in general
-                     as well as a in-depth report on any particular spill."),
-                   h3("Questions"),
-                   p(tags$ol(
-                     tags$li("Which corporations are responsible for the oil spills?"),
-                     tags$li("Where have these oil spills occurred and what kind of pipeline was it?"),
-                     tags$li("Is there a specific kind/type of oil that has been involved in the oil spills?"),
-                     tags$li("What are the causes of these oil spills?"),
-                     tags$li("What was the monetary impact of the spill?")
-                   )
-                   ))
-                 ),
-        tabPanel("Q1"),
-        tabPanel("Q2"),
-        tabPanel("Q3"),
-        tabPanel("Q4"),
-        tabPanel("Q5")
-      
-            
+ui <- fluidPage(
+  includeCSS("www/css/style.css"),
+  navbarPage(
+    "Oil Spill Accident",
+    tabPanel("OverView",
+             img(src = "img/ad62.png", alt = "logo", width = 400, height = 300),
+             mainPanel(
+               tags$div(
+                 h3("Overview of our report"),
+                 p("For the final project, we are working with data of Oil Pipeline Accidents and/or spills in the United
+                   States, from 2010 to present day. The data references the locations of the spills, the corporations
+                   managing the pipelines, causes for the spills, the total costs resulting in these spills, among other
+                   factors. The data used was collected and published by the", a(href = 'https://www.kaggle.com/usdot/pipeline-accidents', "US Department of Transportation", ".")),
+                 p("The data is focused on past spills, the basic details about the spills, including location, date, time,
+                   loss of oil and the impact (in terms of USD) of the oil spills. Looking through the data allows us
+                   insights into patterns and trends of the oil spills and provide a monetary glimpse of their impact. In
+                   learning these things, we will be able to provide to our clients, specific data about oil spills in general
+                   as well as a in-depth report on any particular spill."),
+                 h3("Questions"),
+                 p(tags$ol(
+                   tags$li("Which corporations are responsible for the oil spills?"),
+                   tags$li("Where have these oil spills occurred and what kind of pipeline was it?"),
+                   tags$li("Is there a specific kind/type of oil that has been involved in the oil spills?"),
+                   tags$li("What are the causes of these oil spills?"),
+                   tags$li("What was the monetary impact of the spill?")
+                 )
+                 )
+                 ))
+               ),
+    tabPanel("Q1"),
+    tabPanel("Q2"),
+    tabPanel("Q3"),
+    tabPanel("Q4"),
+    tabPanel("Q5")
 )
+)
+  
+
   
 shinyUI(ui)
